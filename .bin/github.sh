e0e3fd238ba024c147a07d466a475e0ad9833bcf
Danielo Rodriguez <rdanielo@gmail.com>
2018-07-02 08:20:11 +0200

    feat(build): advancing on travis CI

---
+++ .bin/github.sh
@@ -0,0 +1,22 @@
+
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null 2>&1
+  git push --follow-tags --quiet -u origin HEAD 
+}
+
+setup_git
+# commit_website_files
+upload_files
