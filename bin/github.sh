adba6a3e9930c0d087f66b606a6c1a7f07434b55
Danielo Rodriguez <rdanielo@gmail.com>
2018-07-02 09:27:28 +0200

    fix(ci): using branch name for push

--- bin/github.sh
+++ bin/github.sh
@@ -14,7 +14,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add travis https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} # > /dev/null 2>&1
-  git push --follow-tags --quiet -u travis HEAD 
+  git push --follow-tags --quiet travis $TRAVIS_BRANCH 
 }
 
 setup_git
